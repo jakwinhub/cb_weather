365ebffec19d1f520239efcf0a2d29f1dbf33f5a
jakwinhub <jakwin@gmx.de>
2019-07-18 15:13:32 +0200

    Commit intent

--- services.js
+++ services.js
@@ -1,12 +1,13 @@
 const AssistantV1 = require('ibm-watson/assistant/v1');
-const service = new AssistantV1(
+const request = require('request');
+const weather = require('openweather-apis');
+
+const assistant = new AssistantV1(
     {
       version: '2019-02-28',
-      //iam_apikey: 'YAzNj8SfDNMKsgddvaLPFpRCPpqWH0GN1Er2jN2lqp5f',
       username: 'apikey',
       password: 'YAzNj8SfDNMKsgddvaLPFpRCPpqWH0GN1Er2jN2lqp5f',
       url: 'https://gateway-fra.watsonplatform.net/assistant/api'
-     // workspace_id: 'b0922859-da11-4a7c-b549-98e07996b195'
     }
 );
   
@@ -14,23 +15,33 @@ module.exports = {
     MsgToWatson: async function (lineText) {
       console.log("[MsgToWatson]=" + lineText);
         var obj = await new Promise(function (resolve, reject) {
-            service.message({
+          assistant.message({
                 workspace_id: 'b0922859-da11-4a7c-b549-98e07996b195',
                 input: {'text': lineText}
                 })
                 .then(res => {
-                    //console.log("2");
-                  //console.log(JSON.stringify(res, null, 2));
-                  let ret = JSON.stringify(res, null, 4);
-                  console.log (ret);
-                  resolve(res.output.text[0]);
+                  resolve(res);
                 })
                 .catch(err => {
-                    //console.log("3");
-                  //console.log(err)
                   reject('Error while performing Query.');
                 });
         });
         return obj;
+    },
+
+    GetWeather: async function(location) {
+      let city = location;
+      let apiKey = 'e6ecf3ed0b52c40acb6d1ce152e87cf7';
+      var obj = await new Promise(function (resolve, reject) {
+        weather.setLang('en');
+        weather.setCity(location);
+        weather.setUnits('metric');
+        weather.setAPPID(apiKey);
+        weather.getTemperature(function(err, temp){
+          //console.log(temp);
+          resolve(temp);          
+        });
+      });
+      return obj;
     }
 }
